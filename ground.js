e48b7254b2ebcb102e8c20bbe4737cf39cf92b6f
VaibhaviRewari <78912700+VaibhaviRewari@users.noreply.github.com>
2021-04-09 23:12:52 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,26 @@
+class Ground{
+    constructor(x,y,width,height){
+        var options={
+            isStatic:true,
+            'restitution':0,
+            'friction':0,
+            'density':1.0
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+this.width=width;
+this.height=height;
+
+
+World.add(world,this.body);
+
+
+    }
+
+    display(){
+fill(255)
+    
+    rectMode(CENTER)
+    rect(this.body.position.x,this.body.position.y,this.width,this.height);
+
+    }
+}
